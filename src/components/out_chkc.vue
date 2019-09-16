4bbfdf3da7dc77814189a3904ddeef9b7ff21825
cloveropen.com <cloveropen@163.com>
2019-09-16 16:29:53 +0800

    readme

---
+++ src/components/out_chkc.vue
@@ -0,0 +1,303 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-card class="mx-auto" max-width="99%" min-width="100%" justify-center>
+      <v-card-text>
+        <v-layout row wrap >
+          <v-flex d-flex>
+            &emsp;&emsp;
+            <v-text-field
+              v-model="topcode"
+              label="操作员号"
+              required
+            ></v-text-field
+            >&emsp;&emsp;
+          </v-flex>
+          <v-flex d-flex>
+            &emsp;&emsp;
+            <v-text-field
+              v-model="passwd"
+              :rules="passRules"
+              label="密码"
+              required
+              type="password"
+            ></v-text-field
+            >&emsp;&emsp;
+          </v-flex>
+
+          <v-flex d-flex>
+            <v-btn color="success" @click="loginSubmit">验证</v-btn>
+            &emsp;&emsp;
+          </v-flex>
+          <v-row>
+          <v-flex d-flex flex-wrap>
+            &emsp;&emsp;
+            <v-text-field
+              v-model="topname"
+              label="操作员姓名"
+              disabled
+            ></v-text-field
+            >&emsp;&emsp;
+          </v-flex>
+          <br/>
+          <v-flex d-flex>
+            &emsp;&emsp;
+            <v-text-field
+              v-model="tbeg"
+              label="开始时间"
+              disabled
+            ></v-text-field
+            >&emsp;&emsp;
+          </v-flex>
+          <v-flex d-flex>
+            <v-text-field
+              v-model="tend"
+              label="结束时间"
+              disabled
+            ></v-text-field
+            >&emsp;&emsp;
+          </v-flex>
+          </v-row>
+        </v-layout>
+      </v-card-text>
+
+      <v-card-actions class="justify-center">
+        <v-layout row wrap no-gutters>
+          <v-flex d-flex><v-spacer></v-spacer></v-flex>
+          <v-flex d-flex
+            ><v-btn id="snap" :disabled="!valid" color="success">计算</v-btn>
+            <v-btn
+              depressed
+              :disabled="!valid"
+              color="success"
+              @click="validate"
+              >确认交班</v-btn
+            >
+            <v-btn :disabled="!valid" color="success" @click="validate"
+              >打印结算表</v-btn
+            >
+            <v-btn :disabled="!valid" color="success" @click="validate"
+              >打印退款明细</v-btn
+            >
+            <v-spacer></v-spacer
+          ></v-flex>
+        </v-layout>
+      </v-card-actions>
+    </v-card>
+    <v-expansion-panels inset focusable>
+      <v-expansion-panel>
+        <v-expansion-panel-header ripple><b>交班结算表</b></v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <!-- -------------------------交班结算栏 --------------------------------------------- -->
+          <v-data-table
+            :headers="headers"
+            :items="fee_details"
+            :items-per-page="10"
+            class="elevation-1"
+          ></v-data-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-content>
+          <!-- -------------------------退挂号明细栏 --------------------------------------------- -->
+          <v-data-table
+            :headers="headers"
+            :items="fee_details"
+            :items-per-page="10"
+            class="elevation-1"
+          ></v-data-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>退收款明细</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <!-- -------------------------退收款明细栏 --------------------------------------------- -->
+          <v-data-table
+            :headers="headers"
+            :items="fee_details"
+            :items-per-page="10"
+            class="elevation-1"
+          ></v-data-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>退预交金明细</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <!-- -------------------------退预交金明细栏 --------------------------------------------- -->
+          <v-data-table
+            :headers="headers"
+            :items="fee_details"
+            :items-per-page="10"
+            class="elevation-1"
+          ></v-data-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>出院结算召回明细</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <!-- -------------------------出院结算召回明细 --------------------------------------------- -->
+          <v-data-table
+            :headers="headers"
+            :items="fee_details"
+            :items-per-page="10"
+            class="elevation-1"
+          ></v-data-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    topcode: "",
+    topname: "",
+    tpasswd: "",
+    passRules: [
+      v => !!v || "用户密码不能为空",
+      v => v.length >= 6 || "用户密码长度必须超过6个字符"
+    ],
+    tbeg: "",
+    tend: "",
+    headers: [
+      {
+        text: "项目名称",
+        align: "left",
+        sortable: false,
+        value: "item_name"
+      },
+      { text: "单价", value: "real_price" },
+      { text: "数量", value: "quantity" },
+      { text: "天数", value: "days" },
+      { text: "开具时间", value: "cal_time" },
+      { text: "医师", value: "cal_opcode" }
+    ],
+    fee_details: []
+  }),
+
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+    pidChanged(e) {
+      console.log("pid=" + e);
+      //门诊号规则:患者主索引8位，门诊号为11位，门诊号=主索引编号+3位数字，后3位数字为挂号的序号
+      let tpid = e.trim();
+      let _this = this;
+      if (tpid.length == 8 || tpid.length == 11) {
+        let thsp_code = process.env.VUE_APP_HSP_CODE;
+        fetch(
+          process.env.VUE_APP_REG_URL +
+            "/searchoutregcash/" +
+            tpid +
+            "/" +
+            thsp_code,
+          {
+            method: "get",
+            headers: {
+              Accept: "text/html",
+              "Content-Type": "application/json"
+            }
+          }
+        )
+          .then(function(response) {
+            if (response.ok) {
+              //window.alert("---ok=");
+            } else {
+              window.alert("查询患者信息失败error" + response.text);
+            }
+            return response.json();
+          })
+          .then(function(data) {
+            console.log("data=" + JSON.stringify(data));
+            let tresultCode = data.resultCode;
+            //window.alert("tresultCode="+tresultCode)
+            if (tresultCode === "0") {
+              _this.out_reg = JSON.parse(data.outdata);
+              console.log(" this.out_reg=" + JSON.stringify(_this.out_reg));
+              console.log(
+                " this.out_reg.patientName=" + _this.out_reg.patientName
+              );
+              //return toutreg;
+            } else {
+              window.alert("查询患者主索引信息失败1" + data.errorMsg);
+            }
+          })
+          .catch(function(err) {
+            window.alert("查询患者主索引信息查询error=" + err);
+          });
+      } else {
+        //window.alert("请输入正确的门诊号或患者主索引号");
+        return;
+      }
+      console.log(" this.out_reg=" + JSON.stringify(_this.out_reg));
+      return _this.out_reg;
+    },
+    getfeedetail(e) {
+      console.log("getfeedetail pid=" + e);
+      //门诊号规则:患者主索引8位，门诊号为11位，门诊号=主索引编号+3位数字，后3位数字为挂号的序号
+      let tpid = e.trim();
+      let _this = this;
+      if (tpid.length == 8 || tpid.length == 11) {
+        let thsp_code = process.env.VUE_APP_HSP_CODE;
+        fetch(
+          process.env.VUE_APP_REG_URL +
+            "/searchfeedetail/" +
+            tpid +
+            "/9/" +
+            thsp_code,
+          {
+            method: "get",
+            headers: {
+              Accept: "text/html",
+              "Content-Type": "application/json"
+            }
+          }
+        )
+          .then(function(response) {
+            if (response.ok) {
+              //window.alert("---ok=");
+            } else {
+              window.alert("查询患者信息失败error" + response.text);
+            }
+            return response.json();
+          })
+          .then(function(data) {
+            console.log("data=" + JSON.stringify(data));
+            let tresultCode = data.resultCode;
+            //window.alert("tresultCode="+tresultCode)
+            if (tresultCode === "0") {
+              _this.fee_details = JSON.parse(data.outdata);
+              console.log(" this.out_reg=" + JSON.stringify(_this.fee_details));
+            } else {
+              window.alert("查询患者主索引信息失败1" + data.errorMsg);
+            }
+          })
+          .catch(function(err) {
+            window.alert("查询患者主索引信息查询error=" + err);
+          });
+      } else {
+        //window.alert("请输入正确的门诊号或患者主索引号");
+        return;
+      }
+      console.log(" this.out_reg=" + JSON.stringify(_this.out_reg));
+      return _this.out_reg;
+    }
+    // ---------------------end methods----------------
+  }
+};
+</script>
--- src/views/out_chk.vue
+++ src/views/out_chk.vue
@@ -3,7 +3,7 @@
     <Basepage />    
     <v-container fluid fill-height>
       <v-layout justify-start>
-        <test />
+        <out_chkc />
       </v-layout>
     </v-container>
   </v-app>
@@ -11,11 +11,11 @@
 
 <script>
 import Basepage from "../components/Basepage";
-import test from "../components/HelloWorld";
+import out_chkc from "../components/out_chkc";
 export default {
   components: {
     Basepage,
-    test
+    out_chkc
   }
 };
 </script>
