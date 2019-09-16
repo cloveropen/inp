25e55c40a6c5806f18848f5cbdac8f0b63b26bc8
cloveropen.com <cloveropen@163.com>
2019-09-16 10:02:26 +0800

    入院登记

--- src/components/adm_regc.vue
+++ src/components/adm_regc.vue
@@ -495,6 +495,8 @@
           </v-layout>
         </v-card-actions>
       </v-card>
+      
+
     </v-form>
     <canvas ref="canvas" id="canvas" width="640" height="480" hidden></canvas>
     <!-- <ul>
@@ -545,6 +547,24 @@
         </v-row>
       </v-col>
     </v-row>
+<v-card>
+    <v-card-title>
+      入院通知单列表
+      <div class="flex-grow-1"></div>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="按姓名查询"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
   </v-container>
 </template>
 <script>
@@ -652,7 +672,104 @@ export default {
       pic4: "",
       pic5: "",
       pic6: ""
-    }
+    },
+    search: '',
+        headers: [
+          {
+            text: '姓名',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: '门诊号', value: 'calories' },
+          { text: '申请时间', value: 'fat' },
+          { text: '申请医师', value: 'carbs' },
+          { text: '门急诊诊断', value: 'protein' },
+          { text: '患者类型', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ]
+
   }),
   created() {
     this.out_reg.regOpcode = get_regopcode();
